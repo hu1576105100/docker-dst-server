b10293babf468bbfddbbe0b2c21a94017829fad5
hu1576105100 <96818409+hu1576105100@users.noreply.github.com>
2022-08-31 01:29:15 +0800

    Update dedicated_server_mods_setup.lua

--- dst_default_config/DoNotStarveTogether/Cluster_1/mods/dedicated_server_mods_setup.lua
+++ dst_default_config/DoNotStarveTogether/Cluster_1/mods/dedicated_server_mods_setup.lua
@@ -9,3 +9,5 @@
 	--The Workshop id can be found at the end of the url to the collection's Workshop page.
 	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=379114180
 	--ServerModCollectionSetup("379114180")
+ServerModSetup("378160973")
+ServerModSetup("2115943953")
