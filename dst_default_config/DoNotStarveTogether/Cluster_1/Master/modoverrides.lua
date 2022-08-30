f77fc0e550dca32b9f0db09efd4cd9ecedbfee75
hu1576105100 <96818409+hu1576105100@users.noreply.github.com>
2022-08-31 01:22:36 +0800

    Add files via upload

---
+++ dst_default_config/DoNotStarveTogether/Cluster_1/Master/modoverrides.lua
@@ -0,0 +1,14 @@
+return {
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  }
+}
